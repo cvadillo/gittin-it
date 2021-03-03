9bd7f62135fd17f5c21fdc085c77b54b00b10b0f
Carlos V <cavarod@gmail.com>
2021-03-02 16:26:55 -0800

    JS is pulling variable repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,14 @@
 // Get the repo information from GitHub
-var getUserRepos = function() {
-	fetch("https://api.github.com/users/cvadillo/repos");
+var getUserRepos = function(user) {
+	// Format the GitHub api url before making the request
+	var gitApiUrl = "https://api.github.com/users/" + user +"/repos";
+
+	// Assign server response to a variable that we can use
+	fetch("gitApiUrl").then(function(serverResponse) {
+		serverResponse.json().then(function(data) {
+			console.log(data);
+		});
+	});
 };
 
 getUserRepos();
